e98737a1d605f8f0fa392a8753b7dfd557c3c33d
Constup <16638173+constup@users.noreply.github.com>
2021-01-24 02:01:43 +0100

    Improve PhpDocblockTagInterface
    - PhpDocblockTagInterface now extends the ImmutableObjectInterface
    - Symfony var dumper added as dev dependency

--- src/LanguagePhp/Version/Php0702/Component/PhpDocblockTagInterface.php
+++ src/LanguagePhp/Version/Php0702/Component/PhpDocblockTagInterface.php
@@ -4,6 +4,8 @@ declare(strict_types = 1);
 
 namespace Lakobre\Redistributable\LanguagePhp\Version\Php0702\Component;
 
-interface PhpDocblockTagInterface
+use Lakobre\Redistributable\LanguagePhp\Version\Php0702\DesignPatternInterface\ImmutableObjectInterface;
+
+interface PhpDocblockTagInterface extends ImmutableObjectInterface
 {
 }
