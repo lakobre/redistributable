7c75c7fdbe4102994432ced984f806ef7c4df9ca
Constup <16638173+constup@users.noreply.github.com>
2021-01-25 00:01:01 +0100

    Add Module interface

---
+++ src/LanguagePhp/Version/Php0702/DesignPatternInterface/ModuleInterface.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Lakobre\Redistributable\LanguagePhp\Version\Php0702\DesignPatternInterface;
+
+interface ModuleInterface
+{
+    /**
+     * @return string
+     */
+    public function getDataClassName(): string;
+
+    /**
+     * @return string
+     */
+    public function getDataClassInterfaceName(): string;
+
+    /**
+     * @return string
+     */
+    public function getJsonMapperName(): string;
+
+    /**
+     * @return string
+     */
+    public function getValidatorName(): string;
+}
