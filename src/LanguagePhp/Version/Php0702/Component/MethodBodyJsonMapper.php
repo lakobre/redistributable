fdbdbd7e15d0741fab01b3ee55bbc2697dcfd737
Constup <16638173+constup@users.noreply.github.com>
2021-02-16 20:23:41 +0100

    wip

---
+++ src/LanguagePhp/Version/Php0702/Component/MethodBodyJsonMapper.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace Lakobre\Redistributable\LanguagePhp\Version\Php0702\Component;
+
+use stdClass;
+
+class MethodBodyJsonMapper
+{
+    /**
+     * @param object $json
+     * @return null
+     */
+    public function fromJson(object $json)
+    {
+        return null;
+    }
+
+    /**
+     * @param $methodBody
+     * @return object
+     */
+    public function toJson($methodBody): object
+    {
+        return new stdClass();
+    }
+}
